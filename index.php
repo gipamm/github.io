0fd690b3081a732a126e4341a7be83cab0ab4bd7
david_makaridze <dmakaridze@gmail.com>
2019-07-30 23:24:15 +0400

    Generate index.html

--- index.php
+++ index.php
@@ -1,110 +1,204 @@
 <?php
-$list = $arr = [
+$list = [
     [
-        "title" => "My First Salary",
-        "authors" => "Karen Shaybekyan",
-        "mentor" => "Done",
-        "url" => "https://readymag.com/u90183317/1465371/2",
-        "image" => "p1160286.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 1,
+        'title' => 'Grandmother Asli',
+        'authors' => 'Amalya Mgdesyan, Armen Savoyan',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/yyRAd4L9Q5Tyl',
+        'image' => '5677.png',
+        'description' => ''],
     [
-        "title" => "Iron Ladies ",
-        "authors" => "Varduhi Kurghinyan",
-        "mentor" => "Done",
-        "url" => "https://spark.adobe.com/page/1d8eb9a5-1f19-4687-b038-cce093fddcaa/",
-        "image" => "7702.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 2,
+        'title' => 'Library Without Readers',
+        'authors' => 'Hasmik Avagyan, Zhanna Chogandaryan, Aksana Gigolyan',
+        'mentor' => 'Done',
+        'url' => '',
+        'image' => '671.jpg',
+        'description' => ''],
     [
-        "title" => "Woman With a Name of a Flower",
-        "authors" => "Lilit Karakhanyan, Hasmik Avagyan, Zhanna Chogandaryan",
-        "mentor" => "Done",
-        "url" => "https://spark.adobe.com/page/AkEl9j4A5IG2F/",
-        "image" => "9679.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 3,
+        'title' => 'My Phone',
+        'authors' => 'Violetta Sargsyan',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/M6uUXlMHdo7U7/',
+        'image' => '677.jpg',
+        'description' => ''],
     [
-        "title" => "My Phone",
-        "authors" => "Violetta Sargsyan",
-        "mentor" => "Done",
-        "url" => "https://spark.adobe.com/page/M6uUXlMHdo7U7/",
-        "image" => "677.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 4,
+        'title' => 'Iron Ladies ',
+        'authors' => 'Varduhi Kurghinyan',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/1d8eb9a5-1f19-4687-b038-cce093fddcaa/',
+        'image' => '7702.jpg',
+        'description' => ''],
     [
-        "title" => "Smoking ",
-        "authors" => "Eldar",
-        "mentor" => "Done                      ",
-        "url" => "https://spark.adobe.com/page/rPs09VBxgCzt6"],
+        'region' => 'JAVAKHETI',
+        'number' => 5,
+        'title' => 'On His Own ',
+        'authors' => 'Nazeli Khachatryan, Artyom Nalbandyan, Shaliko Nersisyan',
+        'mentor' => 'Taguhi',
+        'url' => '',
+        'image' => '0400.jpg',
+        'description' => ''],
     [
-        "title" => "The Price of Water",
-        "authors" => "Sevak Gabrielyan",
-        "mentor" => "Done",
-        "url" => "https://spark.adobe.com/page/VZRrp88ARJHgv",
-        "image" => "3954.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 6,
+        'title' => 'The Beekeeper',
+        'authors' => 'Veronika Tumasyan',
+        'mentor' => 'Ani',
+        'url' => '',
+        'image' => 'the-bee-keeper.jpg',
+        'description' => ''],
     [
-        "title" => "Grandmother Asli",
-        "authors" => "Amalya Mgdesyan, Armen Savoyan",
-        "mentor" => "Done",
-        "url" => "https://spark.adobe.com/page/yyRAd4L9Q5Tyl",
-        "image" => "5677.png"],
+        'region' => 'JAVAKHETI',
+        'number' => 7,
+        'title' => 'My First Salary',
+        'authors' => 'Karen Shaybekyan',
+        'mentor' => 'Done',
+        'url' => 'https://readymag.com/u90183317/1465371/2',
+        'image' => 'p1160286.jpg',
+        'description' => ''],
     [
-        "title" => "Power of Hands (only video)",
-        "authors" => "Ashkhen Khachatryan",
-        "mentor" => "Done",
-        "url" => "https://www.youtube.com/watch?reload=9&v=iTv6vv51rdA&feature=youtu.be",
-        "image" => "power-of-hand.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 8,
+        'title' => 'The Price of Water',
+        'authors' => 'Sevak Gabrielyan',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/VZRrp88ARJHgv',
+        'image' => '3954.jpg',
+        'description' => ''],
     [
-        "title" => "Library Without Readers",
-        "authors" => "Hasmik Avagyan, Zhanna Chogandaryan, Aksana Gigolyan",
-        "mentor" => "Done",
-        "image" => "671.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 9,
+        'title' => 'Power of Hands (only video)',
+        'authors' => 'Ashkhen Khachatryan',
+        'mentor' => 'Done',
+        'url' => 'https://youtu.be/iTv6vv51rdA',
+        'image' => 'power-of-hand.jpg',
+        'description' => ''],
     [
-        "title" => "On His Own ",
-        "authors" => "Nazeli Khachatryan, Artyom Nalbandyan, Shaliko Nersisyan",
-        "mentor" => "Taguhi",
-        "image" => "0400.jpg"],
+        'region' => 'JAVAKHETI',
+        'number' => 10,
+        'title' => 'Woman With a Name of a Flower',
+        'authors' => 'Lilit Karakhanyan, Hasmik Avagyan, Zhanna Chogandaryan',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/AkEl9j4A5IG2F/',
+        'image' => '9679.jpg',
+        'description' => ''],
     [
-        "title" => "The Beekeeper",
-        "authors" => "Veronika Tumasyan",
-        "mentor" => "Ani",
-        "image" => "the-bee-keeper.jpg"],
+        'region' => 'MARNEULI',
+        'number' => 11,
+        'title' => 'Class minus girls',
+        'authors' => 'Namig, Amil, Nicat',
+        'mentor' => 'Not yet  ',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Class minus girls",
-        "authors" => "Namig, Amil, Nicat",
-        "mentor" => "Not yet  "],
+        'region' => 'MARNEULI',
+        'number' => 12,
+        'title' => 'Garbage ',
+        'authors' => 'Kenan and Senan',
+        'mentor' => 'Done ',
+        'url' => 'https://spark.adobe.com/page/pVKYFMppooM7A',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Garbage ",
-        "authors" => "Kenan and Senan",
-        "mentor" => "Done "],
+        'region' => 'MARNEULI',
+        'number' => 13,
+        'title' => 'Early marriage ',
+        'authors' => 'Gulgun',
+        'mentor' => 'Not yet (in a hour) ',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Early marriage ",
-        "authors" => "Gulgun",
-        "mentor" => "Not yet (in a hour) "],
+        'region' => 'MARNEULI',
+        'number' => 14,
+        'title' => 'Vanising trees',
+        'authors' => 'Eltun',
+        'mentor' => 'Done ',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Vanising trees",
-        "authors" => "Eltun",
-        "mentor" => "Done "],
+        'region' => 'MARNEULI',
+        'number' => 15,
+        'title' => 'Smoking ',
+        'authors' => 'Eldar',
+        'mentor' => 'Done                      ',
+        'url' => 'https://spark.adobe.com/page/rPs09VBxgCzt6',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Transport ",
-        "authors" => "Aziz , Sema",
-        "mentor" => "Done "],
+        'region' => 'MARNEULI',
+        'number' => 16,
+        'title' => 'Transport ',
+        'authors' => 'Aziz , Sema',
+        'mentor' => 'Done ',
+        'url' => 'https://spark.adobe.com/page/vRt3NVJSWlLyw',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Meet  Abbas ",
-        "authors" => "Idris ",
-        "mentor" => "Done"],
+        'region' => 'MARNEULI',
+        'number' => 17,
+        'title' => 'Meet  Abbas ',
+        'authors' => 'Idris ',
+        'mentor' => 'Done',
+        'url' => 'https://spark.adobe.com/page/ACvAOqTApXher',
+        'image' => '',
+        'description' => ''],
     [
-        "authors" => "Xuldara "],
+        'region' => 'MARNEULI',
+        'number' => 18,
+        'title' => '',
+        'authors' => 'Xuldara ',
+        'mentor' => '',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Garage ",
-        "authors" => "Vali, Mehemmed ",
-        "mentor" => "Done"],
+        'region' => 'MARNEULI',
+        'number' => 19,
+        'title' => 'Garage ',
+        'authors' => 'Vali, Mehemmed ',
+        'mentor' => 'Done',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Wishing trees ",
-        "authors" => "Gunel, Bahar",
-        "mentor" => "Done"],
+        'region' => 'MARNEULI',
+        'number' => 20,
+        'title' => 'Wishing trees ',
+        'authors' => 'Gunel, Bahar',
+        'mentor' => 'Done',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "On my way home",
-        "authors" => "Tural",
-        "mentor" => "Done"],
+        'region' => 'MARNEULI',
+        'number' => 21,
+        'title' => 'On my way home',
+        'authors' => 'Tural',
+        'mentor' => 'Done',
+        'url' => '',
+        'image' => '',
+        'description' => ''],
     [
-        "title" => "Dogs ",
-        "authors" => "Ayxan , Kenan ",
-        "mentor" => "Done"]
-];;
+        'region' => 'MARNEULI',
+        'number' => 22,
+        'title' => 'Dogs ',
+        'authors' => 'Ayxan , Kenan ',
+        'mentor' => 'Done',
+        'url' => '',
+        'image' => '',
+        'description' => '']
+];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -114,8 +208,8 @@ $list = $arr = [
     <title>Title</title>
     <!-- styles -->
     <link rel="stylesheet" href="css/Reset.css">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="css/Layout.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://newscafe.ge/Content/Layout.css">
     <link rel="stylesheet" href="css/Fonts.css">
     <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="css/all.css">
@@ -130,7 +224,6 @@ $list = $arr = [
                 Interactive
             </header>
             <?php foreach ($list as $key => $item): ?>
-                <?php if (key_exists('url', $item) AND $item['url'] !== ''):?>
                 <div class="col-xs-10 col-md-8 offset-md-2 blockDesign blockDesign<?php echo $key % 4 + 1; ?>">
                     <div class="parent">
                         <div class="introImg" style="background-image:url(<?php echo 'images/' . $item['image']; ?>);">
@@ -147,14 +240,14 @@ $list = $arr = [
                     <h4 class="authors"><?php echo $item['authors']; ?></h4>
                     <div class="animationBox"></div>
                 </div>
-                <?php endif;?>
             <?php endforeach; ?>
         </div>
     </div>
 </div>
 <!-- scripts -->
-<script src="node_modules/jquery/dist/jquery.js"></script>
-<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <!-- end scripts -->
 </body>
 </html>
